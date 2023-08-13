899a3bf181e53b1361b55667dd8abff4455b8fbf
koopichi <82188509+koopichi@users.noreply.github.com>
2023-08-14 00:22:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ lnd
 
 sed -i "s/_;/$DOMAIN;/" "/etc/nginx/sites-available/$DOMAIN"
 sed -i "s/ default_server//" "/etc/nginx/sites-available/$DOMAIN"
-sed -i "53 r /root/ReverseProxyN/reverse.txt" "/etc/nginx/sites-available/$DOMAIN"
+sed -i "53 r /root/reverse/reverse.txt" "/etc/nginx/sites-available/$DOMAIN"
 
 certbot --nginx -d $DOMAIN --register-unsafely-without-email
 
